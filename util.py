fe11ba56b735f2cab018f1e2dc423ac5d7624ac1
ahijevyc <ahijevyc@ucar.edu>
2024-07-29 15:56:56 -0600

    Use uxarray methods more.
    subset
    
    remap

---
+++ util.py
@@ -0,0 +1,62 @@
+import cartopy
+import numpy as np
+import xarray
+from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
+
+def mkcoord(ds):
+    if "t_iso_levels" in ds:
+        ds = ds.swap_dims(dict(nIsoLevelsT="t_iso_levels", nIsoLevelsZ="z_iso_levels"))
+    return ds
+
+
+def dec_ax(ax, extent):
+    ax.add_feature(cartopy.feature.STATES)
+    ax.set_extent(extent)
+
+    gl = ax.gridlines(draw_labels=True, x_inline=False)
+    gl.top_labels = gl.right_labels = False
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+
+
+def dBZfunc(dBZ, func):
+    """mean of linearized Z, not logarithmic dbZ"""
+    Z = 10 ** (dBZ / 10)
+    fZ = func(Z)
+    return np.log10(fZ) * 10
+
+
+def trim_ll(grid_path, data_paths, lon_bounds, lat_bounds):
+    """
+    trim grid file and data file to bounds
+    SLOW: compare to uxarray.Grid.subset
+    """
+
+    # Open the Grid file
+    grid_ds = xarray.open_dataset(grid_path)
+
+    grid_ds["lonCell"] = np.degrees(grid_ds.lonCell)
+    grid_ds["latCell"] = np.degrees(grid_ds.latCell)
+
+    # before computing the triangulation
+    grid_ds["lonCell"] = ((grid_ds["lonCell"] + 180) % 360) - 180
+
+    # Open data files
+    ds = xarray.open_mfdataset(
+        data_paths, preprocess=mkcoord, concat_dim="Time", combine="nested"
+    )
+    lon0, lon1 = lon_bounds
+    lat0, lat1 = lat_bounds
+    ibox = (
+        (grid_ds.lonCell >= lon0)
+        & (grid_ds.lonCell < lon1)
+        & (grid_ds.latCell >= lat0)
+        & (grid_ds.latCell < lat1)
+    )
+    # Trim grid
+    grid_ds = grid_ds[["latCell", "lonCell"]].where(ibox, drop=True)
+
+    # Trim data
+    ds = ds.where(ibox, drop=True)
+
+    return grid_ds, ds
